0d9bca3ab7e5c9e4dd074360a59ea18834eb0dae
Jason-Cee <jasoncee017@gmail.com>
2021-08-02 16:31:05 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, request
+import hmac
+import sqlite3
+
+from flask_jwt import JWT, jwt_required, current_identity
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+def fetch_users():
+    with sqlite3.connect('sale.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+
+        new_data = []
+
+        for data in users:
+            new_data.append(User(data[0], data[3], data[4]))
+    return new_data
+
+
+def init_user_table():
+    conn = sqlite3.connect('sale.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "first_name TEXT NOT NULL,"
+                 "last_name TEXT NOT NULL,"
+                 "username TEXT NOT NULL,"
+                 "password TEXT NOT NULL)")
+    print("user table created successfully")
+    conn.close()
+
+
+def init_item_table():
+    with sqlite3.connect('sale.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "title TEXT NOT NULL,"
+                     "category TEXT NOT NULL,"
+                     "price INTEGER NOT NULL)")
+    print("item table created successfully.")
+
+
+init_user_table()
+init_item_table()
+users = fetch_users()
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and hmac.compare_digest(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+CORS(app)
+
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+
+@app.route('/registration/', methods=["POST"])
+def user_registration():
+    response = {}
+
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        username = request.form['username']
+        password = request.form['password']
+
+        with sqlite3.connect("sale.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users("
+                           "first_name,"
+                           "last_name,"
+                           "username,"
+                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
+            conn.commit()
+            response["message"] = "success"
+            response["status_code"] = 200
+        return response
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
